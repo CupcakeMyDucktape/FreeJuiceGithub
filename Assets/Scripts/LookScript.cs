96940c7430d3b1798a58fb477a5ad38b16517a3d
Shade Emsigima <gamingkendoka@gmail.com>
2019-10-16 15:52:37 +0200

    Look around
    - player can now look around using mouse

---
+++ Assets/Scripts/LookScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookScript : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Mouse X") < 0)
+            transform.Rotate(Vector3.up* speed);
+        if (Input.GetAxis("Mouse X") > 0)
+            transform.Rotate(Vector3.down* speed);
+    }
+}
