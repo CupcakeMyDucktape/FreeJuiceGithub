923b02251b16a4462488965e57de9b43dacf7c70
Shade Emsigima <gamingkendoka@gmail.com>
2019-10-16 16:46:23 +0200

    Tried adding player feedback scene 3, failed

--- Assets/Scripts/PlaySoundOnTrigger.cs
+++ Assets/Scripts/PlaySoundOnTrigger.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
-//pay me a certain sound the sound component on this gameobject is carrying plx.
+//play me a certain sound the sound component on this gameobject is carrying plx.
 
 public class PlaySoundOnTrigger : MonoBehaviour
 {
 
     AudioSource theSound;
 
-    void OnTriggerEnter(Collider other) {
-        theSound = GetComponent<AudioSource>();
-        theSound.Play(0);
-        Debug.Log(other.name + "entered the water, playing sound");   
+    void Update() {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W))
+        {
+            theSound = GetComponent<AudioSource>();
+            theSound.Play(0);
+            Debug.Log("PLaying sound");
+        }
     }
 }
